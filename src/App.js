291ddc8b925518cf8511a3092aec19eb265b6286
avulakunta balu <avulakuntabalu11@gmail.com>
2023-06-08 13:14:27 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <div>
+      <center> 
+       <h1>react deploy</h1>
+       <img src='allfood.png' width={300} height={300} />
+      </center>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
+ 
