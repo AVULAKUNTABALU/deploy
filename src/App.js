e2dc7e8bc95cd10f922b8b81b89c29aa18bcd6c8
avulakunta balu <avulakuntabalu11@gmail.com>
2023-06-08 13:18:38 +0530

    2 code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@
       <center> 
        <h1>react deploy</h1>
        <img src='allfood.png' width={300} height={300} />
+       <p>image teken by balu vaddi gani kalyani hari inadhira srinivasulu laxmi devi lavanya vani sunitha nani teju suresh sahasra
+       </p>
       </center>
      </div>
    )
